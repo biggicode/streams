d020f4c58417d61093b6819d4e5ae5833712b1a2
biggicode <larioncescu@yahoo.com>
2022-03-09 18:03:50 +0200

    add routes

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -1,31 +1,21 @@
 import React from "react"
 import { BrowserRouter, Route, Link } from "react-router-dom"
-
-const PageOne = () => {
-  return (
-    <div>
-      page one
-      <Link to="/pagetwo">Navigate to page two</Link>
-    </div>
-  )
-}
-const PageTwo = () => {
-  return (
-    <div>
-      page Two
-      <button>Click Me!</button>
-      <Link to="/">Navigate to page one</Link>
-    </div>
-  )
-}
+import StreamList from "../stream-list"
+import StreamShow from "../stream-show"
+import StreamCreate from "../stream-create"
+import StreamDelete from "../stream-delete"
+import StreamEdit from "../stream-edit"
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
