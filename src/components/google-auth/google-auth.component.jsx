5f8aa4ffbb8ec89ade1758d19aa719cf55a25b20
biggicode <larioncescu@yahoo.com>
2022-03-10 20:11:51 +0200

    add event handlers

--- src/components/google-auth/google-auth.component.jsx
+++ src/components/google-auth/google-auth.component.jsx
@@ -22,19 +22,27 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() })
   }
 
+  onSingIn = () => {
+    this.auth.signIn()
+  }
+
+  onSignOut = () => {
+    this.auth.signOut()
+  }
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null
     } else if (this.state.isSignedIn) {
       return (
-        <button className="ui red google button">
+        <button onClick={this.onSignOut} className="ui red google button">
           <i className="google icon" />
           Sing Out
         </button>
       )
     } else {
       return (
-        <button className="ui red google button">
+        <button onClick={this.onSingIn} className="ui red google button">
           <i className="google icon" />
           Sing In With Google
         </button>
