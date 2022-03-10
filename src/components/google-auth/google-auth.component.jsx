517af79d0c65a0496a95eb0b3afcb9c1be0edf37
biggicode <larioncescu@yahoo.com>
2022-03-10 20:27:08 +0200

    setup redux

--- src/components/google-auth/google-auth.component.jsx
+++ src/components/google-auth/google-auth.component.jsx
@@ -22,11 +22,11 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() })
   }
 
-  onSingIn = () => {
+  onSingInClick = () => {
     this.auth.signIn()
   }
 
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut()
   }
 
@@ -35,14 +35,14 @@ class GoogleAuth extends React.Component {
       return null
     } else if (this.state.isSignedIn) {
       return (
-        <button onClick={this.onSignOut} className="ui red google button">
+        <button onClick={this.onSignOutClick} className="ui red google button">
           <i className="google icon" />
           Sing Out
         </button>
       )
     } else {
       return (
-        <button onClick={this.onSingIn} className="ui red google button">
+        <button onClick={this.onSingInClick} className="ui red google button">
           <i className="google icon" />
           Sing In With Google
         </button>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react"
 import { render } from "react-dom"
 import App from "./components/app"
+import { Provider } from "react-redux"
+import { createStore } from "redux"
 
-const app = <App />
+import reducers from "./reducers"
+
+const store = createStore(reducers)
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 const root = document.querySelector("#root")
 
 render(app, root)
