a6f3b65329079305e744729094fd7df42c7c3e2b
biggicode <larioncescu@yahoo.com>
2022-03-18 14:08:00 +0200

    make component more reusable

--- src/components/stream-delete/stream-delete.component.jsx
+++ src/components/stream-delete/stream-delete.component.jsx
@@ -2,10 +2,21 @@ import React from "react"
 import Modal from "../modal"
 
 const StremDelete = () => {
+  const actions = (
+    <React.Fragment>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>
+  )
+
   return (
     <div>
       StremDelete
-      <Modal />
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
     </div>
   )
 }
